e87e8482fa3f9f2990f0c0f629f916c1e123bd56
Samer Saied <samerpasha@hotmail.com>
2023-08-15 06:42:42 +0300

    Frist version

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       apiToken: "z5Bq2TFvQhrNXuJcfbLIEXWu",
-      roles: ["strapi-super-admin"],
+      deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_qdbzaOpA1s2rXpfvrPbuLVyAW835/PQolDuMT9v",
+      // roles: ["strapi-super-admin"],
     },
   },
 });
